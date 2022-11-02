65636cb652b144c43b8c026c675de63e1e31983e
AllaKulchitskaya <alla070@list.ru>
2022-11-02 18:09:49 +0300

    Задача 1

---
+++ src/Task1.java
@@ -0,0 +1,12 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int age1 = 24;
+        if (age1 >= 18) {
+            System.out.println("Поздравляем! Вам " + age1 + " года. Вы совершеннолетний");
+        }
+        int age2 = 15;
+        if (age2 < 18) {
+            System.out.println("Вам " + age2 + " лет. Возраст совершеннолетия еще не наступил. Нужно немного подождать");
+        }
+    }
+}
