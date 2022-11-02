3f0f3d3d0e64d302684688e4fda1672fe7dfb335
AllaKulchitskaya <alla070@list.ru>
2022-11-02 18:29:28 +0300

    Задача 1, переписанная с else

---
+++ src/Task1_rewrite.java
@@ -0,0 +1,10 @@
+public class Task1_rewrite {
+    public static void main(String[] args) {
+        int age = 24;
+        if (age >= 18) {
+            System.out.println("Поздравляем! Вы совершеннолетний");
+        } else {
+            System.out.println("Возраст совершеннолетия еще не наступил. Нужно немного подождать");
+        }
+    }
+}
